de20af89d25a98968107b859bbf7c10b6bd40d28
ericettensohn <ericettensohn@gmail.com>
2016-08-18 15:26:57 -0400

    4

--- js/controllers/insta-controller.js
+++ js/controllers/insta-controller.js
@@ -1,8 +1,10 @@
 app.controller('instaController', function($scope, $http){
 	var feed = new Instafeed({
-	    get: 'tagged',
-	    tagName: 'awesome',
-	    clientId: 'cocacola'
+	    get: 'user',
+	    userId: 249655166,
+	    sortBy: 'most-recent',
+	    limit: 12,
+	    clientId: '1786183730.8b81590.16aa19bacf7d4d418843d962627ddf5f'
     });
     feed.run();
 });
