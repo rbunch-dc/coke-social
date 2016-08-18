3e958cf1856cf261ba383c929e06b920045f3ea6
Jackson Yeager <jacksonyeager@gmail.com>
2016-08-18 14:31:59 -0400

    Added twitter directive

--- js/app.js
+++ js/app.js
@@ -2,3 +2,10 @@
  * Created by Jackson on 8/18/16.
  */
 var app = angular.module('cokeApp', ['ngRoute']);
+
+app.directive('twitter', function(){
+    return {
+        restrict: 'E',
+        templateUrl: 'js/directives/twitter.html'
+    }
+});
