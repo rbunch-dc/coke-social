edd278331011f90449cf2ff48f8b0ccba47a55f1
summeradhal <summeradhal@gmail.com>
2016-08-18 14:43:31 -0400

    tweet controller

--- js/controllers/twitter-controller.js
+++ js/controllers/twitter-controller.js
@@ -2,5 +2,16 @@
  * Created by Jackson on 8/18/16.
  */
 app.controller('twitterController', function($scope){
-    $scope.twitter = "Hello World!";
+  
+  $http({
+        method: 'GET',
+        url: 'http://digitalcrafts.com/students/twitter/hashtag.php?user=true&hash=coke'
+    }).then(function successFunction(tweetData){
+            $scope.tweets = tweetData.data.statuses;
+            console.dir($scope.tweets);
+        }, function failureFunction(tweetData){
+            console.log(tweetData);
+        }
+    );
+    
 });
