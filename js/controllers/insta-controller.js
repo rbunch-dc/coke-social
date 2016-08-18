ffd2d667d77999195322ac5bcf9f3d2b94dded4b
ericettensohn <ericettensohn@gmail.com>
2016-08-18 15:54:41 -0400

    bunch of stuff

--- js/controllers/insta-controller.js
+++ js/controllers/insta-controller.js
@@ -1,10 +1,19 @@
 app.controller('instaController', function($scope, $http){
+	
+	app.directive('instagram', function(){
+		return {
+			restrict: 'E',
+			templateUrl: 'js/directives/insta-view.html'
+		}
+	});
+
 	var feed = new Instafeed({
 	    get: 'user',
-	    userId: 249655166,
+	    userId: '249655166',
 	    sortBy: 'most-recent',
 	    limit: 12,
-	    clientId: '1786183730.8b81590.16aa19bacf7d4d418843d962627ddf5f'
+	    // clientId: '8b81590906e04825a1d28b276caa1bdf'
+	    accessToken: '1786183730.8b81590.16aa19bacf7d4d418843d962627ddf5f'
     });
     feed.run();
 });
