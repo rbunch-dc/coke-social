880f6f40dffe5e9a6cfee5e315ad7fd8492044dd
Jackson Yeager <jacksonyeager@gmail.com>
2016-08-18 14:42:36 -0400

    Fixed controller

--- js/controllers/twitter-controller.js
+++ js/controllers/twitter-controller.js
@@ -1,6 +1,15 @@
 /**
  * Created by Jackson on 8/18/16.
  */
-app.controller('twitterController', function($scope){
-    $scope.twitter = "Hello World!";
+app.controller('twitterController', function($scope, $http){
+    $http({
+        method: 'GET',
+        url: 'http://digitalcrafts.com/students/twitter/hashtag.php?user=true&hash=cokecola'
+    }).then(function successFunction(tweetData){
+            $scope.tweets = tweetData.data.statuses;
+            console.dir($scope.tweets);
+        }, function failureFunction(tweetData){
+            console.log(tweetData);
+        }
+    );
 });
