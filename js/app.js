dbed94ac5d39bad1facb702e56415cc373f2a651
Jackson Yeager <jacksonyeager@gmail.com>
2016-08-18 12:37:57 -0400

    Added boilerplate for angular app and compass.

---
+++ js/app.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Jackson on 8/18/16.
+ */
+var app = angular.module('cokeApp', ['ngRoute']);
---
+++ js/controllers/coke-controller.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Jackson on 8/18/16.
+ */
+app.controller('cokeController', function($scope){
+    $scope.helloWorld = "Hello, world!";
+});
